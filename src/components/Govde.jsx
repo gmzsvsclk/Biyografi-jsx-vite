cc6f4d8bdd6c5a4a1f38e0c1b480c0d6bb75b57d
Gamze S.Celik <gamze_savas@hotmail.com>
2024-02-09 13:19:23 +0300

    Biyografi-Sayfa (5.Hafta)

---
+++ src/components/Govde.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Govde() {
+  return (
+    <div>
+      <header className="header">Blok Sayfa</header>
+      <div className="photo">
+        <img className="mainPicture" src="src/components/images/foto2.jpg" alt="Avatar"></img>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Text.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Text=()=> {
+  return (
+    <div className='text'>
+       <center>
+       <h2>Gamze Savaş Çelik</h2>
+       
+        <p>1991 yılında Bursa da doğdum. Tüm eğitim-öğretim yılında Bursadaydım. 
+          Bursa Uludağ Üniversitesi Matematik bölümünü 3 yılda tamamlayarak 2013 yılında mezun oldum.</p>
+           <p>Bursa Uludağ Üniversitesi Matematik bölümünü Cebir ve Sayılar teorisi alanında 2016 yılında yüksek lisansımı,
+            2022 yılında doktoramı tamamladım.
+           Doktora sürecinde Bursa Teknik Üniversitesinde misafir öğretim görevlisi olarak görev yaptım.
+           Evliyim, 1.5 ve 4.5 yaşlarında iki erkek evlat sahibiyim.
+           Şuanda hem bebeğimi büyütüyorum hem de Üretken akademi işbirliği ile düzenlenen yazılım eğitimi alıyorum.</p>
+       </center>
+
+    </div>
+  )
+}
+
+
+export default Text;
